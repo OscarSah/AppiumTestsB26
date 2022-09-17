e720d9d1118df841f6708bdf5bb1a59d7bb1288c
OscarSah <oscar@cybertekschool.com>
2022-09-17 19:42:44 +0300

    Framework Elements

--- src/test/java/com/cydeo/pages/CalculatorPage.java
+++ src/test/java/com/cydeo/pages/CalculatorPage.java
@@ -4,6 +4,7 @@ import com.cydeo.utils.Driver;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.pagefactory.AndroidFindBy;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import org.openqa.selenium.By;
 import org.openqa.selenium.support.PageFactory;
 
 public class CalculatorPage {
@@ -11,7 +12,22 @@ public class CalculatorPage {
         PageFactory.initElements(new AppiumFieldDecorator(Driver.getDriver()),this);
     }
 
-    @AndroidFindBy()
+    @AndroidFindBy(accessibility = "multiply")
     public MobileElement multiply;
+
+    @AndroidFindBy(accessibility = "equals")
+    public MobileElement equals;
+
+    @AndroidFindBy(id = "com.google.android.calculator:id/result_final")
+    public MobileElement result;
+
+    //
+    //com.google.android.calculator:id/digit_4
+
+    public void clickDigit(int digit){
+        String id = "com.google.android.calculator:id/digit_"+digit;
+        MobileElement number = Driver.getDriver().findElement(By.id(id));
+        number.click();
+    }
 
 }
