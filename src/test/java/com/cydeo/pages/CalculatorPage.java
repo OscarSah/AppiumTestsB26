adc0ca09e1cdd650047bff28fffa7fbf1dddccad
OscarSah <oscar@cybertekschool.com>
2022-09-17 19:31:59 +0300

    Framework Elements

--- src/test/java/com/cydeo/pages/CalculatorPage.java
+++ src/test/java/com/cydeo/pages/CalculatorPage.java
@@ -1,4 +1,17 @@
 package com.cydeo.pages;
 
+import com.cydeo.utils.Driver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.pagefactory.AndroidFindBy;
+import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import org.openqa.selenium.support.PageFactory;
+
 public class CalculatorPage {
+    public CalculatorPage(){
+        PageFactory.initElements(new AppiumFieldDecorator(Driver.getDriver()),this);
+    }
+
+    @AndroidFindBy()
+    public MobileElement multiply;
+
 }
