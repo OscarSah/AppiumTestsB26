cf3a91de9bcc4dc5dd533dd56d406cef766e39ba
OscarSah <oscar@cybertekschool.com>
2022-09-17 18:48:26 +0300

    First Test Case

--- src/test/java/com/cydeo/tests/CalculatorTest.java
+++ src/test/java/com/cydeo/tests/CalculatorTest.java
@@ -1,10 +1,13 @@
 package com.cydeo.tests;
 
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileBy;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.net.MalformedURLException;
@@ -32,6 +35,30 @@ public class CalculatorTest {
         AppiumDriver<MobileElement> driver = new AndroidDriver<>(url,caps);
 
         Thread.sleep(4000);
+
+        MobileElement digit9 = driver.findElement(By.id("com.google.android.calculator:id/digit_9"));
+        digit9.click();
+
+        Thread.sleep(1000);
+        MobileElement clearElem= driver.findElement(MobileBy.AccessibilityId("clear"));
+        System.out.println("clearElem.getText() = " + clearElem.getText());
+        Assertions.assertTrue(clearElem.isDisplayed());
+        clearElem.click();
+
+        /*
+        This is with Inspector Recording
+MobileElement el7 = (MobileElement) driver.findElementById("com.google.android.calculator:id/digit_9");
+el7.click();
+MobileElement el8 = (MobileElement) driver.findElementByAccessibilityId("plus");
+el8.click();
+MobileElement el9 = (MobileElement) driver.findElementById("com.google.android.calculator:id/digit_5");
+el9.click();
+MobileElement el10 = (MobileElement) driver.findElementByAccessibilityId("equals");
+el10.click();
+
+         */
+
+
 
         driver.closeApp();
 
